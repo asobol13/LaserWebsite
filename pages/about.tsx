58e2902600453ff5333dc51abfb1e1bc17569bfd
Allison Sobol <allisonsobol13@gmail.com>
2023-10-17 14:44:31 -0700

    Added about and gallery pages

---
+++ pages/about.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import styles from '../styles/index.css';
+
+function About () {
+    return (
+        <h1>About Page</h1>
+    );
+}
+
+export default About;
---
+++ pages/gallery.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import styles from '../styles/index.css';
+
+function Gallery () {
+    return (
+        <h1>Gallery Page</h1>
+    );
+}
+
+export default Gallery;
