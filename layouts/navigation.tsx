be2bb18d0501ad887b1b650d8f7547dd82cd57a8
Allison Sobol <allisonsobol13@gmail.com>
2023-10-17 20:17:35 -0700

    Added nav bar to all pages

--- layouts/navigation.tsx
+++ layouts/navigation.tsx
@@ -12,9 +12,9 @@ function Navigation () {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div className="navbar-nav">
-                    <a className="nav-link active" aria-current="page" href="#">Home</a>
-                    <a className="nav-link" href="#">About</a>
-                    <a className="nav-link" href="#">Gallery</a>
+                    <a className="nav-link" aria-current="page" href="#">Home</a>
+                    <a className="nav-link" href="/about">About</a>
+                    <a className="nav-link" href="/gallery">Gallery</a>
                 </div>
                 </div>
             </div>
--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 // import styles from '../assets/index.css';
+import Navigation from '../layouts/navigation';
 
 function Gallery () {
     return (
-        <h1>Gallery Page</h1>
+        <div className="entire-container">
+            <div className="container">
+                <Navigation />
+            </div>
+            <div className="container">
+                <h1>Gallery Page</h1>
+            </div>
+        </div>
     );
 }
 
