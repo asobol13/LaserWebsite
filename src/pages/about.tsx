93fc6ff4c6e10ea5b06a2533fe0b87300908559d
Allison Sobol <allisonsobol13@gmail.com>
2023-12-03 18:38:57 -0800

    working on panels

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,73 +1,72 @@
-import React, { CSSProperties, useState } from 'react';
-import Navigation from '../layouts/navigation';
-import { useSpring, animated } from 'react-spring';
-import Footer2 from '../layouts/footer2';
+import React, { CSSProperties } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigation from '../layouts/navigation';
+import Footer2 from '../layouts/footer2';
 
-// TODO:
-// 1) Add Immersive photo slide experience
-// 2) Add a good story telling aspect
-
-const pageStyle: CSSProperties = {
-    backgroundColor: '#BFCBE0',
-    minHeight: '100vh', // Ensure the page takes up at least the height of the viewport
+const aboutContainerStyle: CSSProperties = {
     display: 'flex',
     flexDirection: 'column',
+    alignItems: 'center',
 };
 
-function About () {
-    const [isVisible, setIsVisible] = useState(false);
+const aboutPanelStyle = {
+    width: '80%',
+    padding: '20px',
+    margin: '20px 0',
+    border: '1px solid #ddd',
+    borderRadius: '8px',
+    opacity: 0,
+    transform: 'translateY(20px)',
+    transition: 'opacity 0.5s, transform 0.5s',
+};
 
-    const slideIn = useSpring({
-        opacity: isVisible ? 1 : 0,
-        transform: isVisible ? 'translateY(0)' : 'translateY(100%)',
-    });
+const panel1Style = {
+    backgroundColor: '#FAD02E', // Yellow
+};
 
-    // Function to handle image onLoad event and trigger the slide-up animation
-    const handleImageLoad = () => {
-        setIsVisible(true);
-    };
+const panel2Style = {
+    backgroundColor: '#66BB6A', // Green
+};
 
-    return (
-        <div className="entire-container" style={pageStyle}>
-            <div className="container">
-                <Navigation />
-            </div>
-            <div className="container">
-                <h1 style={{ textAlign: 'center' }}>Who We Are</h1>
-            </div>
-            <animated.div style={slideIn}>
-                <div className="container" style={{ minHeight: '400px' }}>
-                    <img
-                        src="https://m.media-amazon.com/images/I/61VcdmN8qbL._AC_UF894,1000_QL80_.jpg"
-                        className="img-fluid"
-                        alt="Responsive image"
-                        onLoad={handleImageLoad}
-                        style={{ width: '100%' }}
-                    />
-                </div>
-                <div className="container" style={{ minHeight: '400px' }}>
-                    <img
-                        src="https://m.media-amazon.com/images/I/61VcdmN8qbL._AC_UF894,1000_QL80_.jpg"
-                        className="img-fluid"
-                        alt="Responsive image"
-                        onLoad={handleImageLoad}
-                        style={{ width: '100%' }}
-                    />
-                </div>
-                <div className="container" style={{ minHeight: '400px' }}>
-                    <img
-                        src="https://m.media-amazon.com/images/I/61VcdmN8qbL._AC_UF894,1000_QL80_.jpg"
-                        className="img-fluid"
-                        alt="Responsive image"
-                        onLoad={handleImageLoad}
-                        style={{ width: '100%' }}
-                    />
-                </div>
-            </animated.div>
-            {/* <Footer /> */}
+const panel3Style = {
+    backgroundColor: '#4FC3F7', // Blue
+};
+
+const AboutPage = () => {
+  return (
+    <div className="entire-container">
+        <div className="container">
+            <Navigation />
         </div>
-    );
-}
+        <div className="aboutContainer" style={aboutPanelStyle}>
+            <div className="about-panel panel-1" style={panel1Style}>
+                <h2>Our Story</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero,
+                    a pharetra augue. Vestibulum id ligula porta felis euismod semper. Praesent commodo
+                    cursus magna, vel scelerisque nisl consectetur et.
+                </p>
+            </div>
 
-export default About;
+            <div className="about-panel panel-2" style={panel2Style}>
+                <h2>Meet the Team</h2>
+                <p>
+                    Vestibulum id ligula porta felis euismod semper. Duis mollis, est non commodo luctus,
+                    nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec sed odio dui.
+                </p>
+            </div>
+
+            <div className="about-panel panel-3" style={panel3Style}>
+                <h2>Our Values</h2>
+                <p>
+                    Maecenas faucibus mollis interdum. Aenean lacinia bibendum nulla sed consectetur.
+                    Nullam id dolor id nibh ultricies vehicula ut id elit.
+                </p>
+            </div>
+        </div>
+        <Footer2 />
+    </div>
+  );
+};
+
+export default AboutPage;
